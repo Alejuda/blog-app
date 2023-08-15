0a23f92f91f073f78f25a219c947952cd1353fc9
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-08-15 19:49:28 -0300

    Add recent_posts for User and recet_comments for Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes, foreign_key: :post_id
   has_many :comments, foreign_key: :post_id
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(2)
+  end
 end
