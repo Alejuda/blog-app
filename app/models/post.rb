6b8b5858dc50b52cc84faa0df18b8234502c7152
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-08-16 14:28:40 -0300

    Fix linters errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,13 +4,13 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
 
   after_save :update_user_posts_counter
-  
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
   private
-  
+
 
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count)
