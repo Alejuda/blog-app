32aac40bd9ce45dcab442c7a957045ce06e806ab
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-08-16 14:28:11 -0300

    Add required changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,14 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: :post_id
   has_many :comments, foreign_key: :post_id
 
+  after_save :update_user_posts_counter
+  
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
+
+  private
+  
 
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count)
