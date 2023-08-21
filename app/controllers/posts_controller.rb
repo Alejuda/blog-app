fc93a2db769355627abeae6977bfd1dc4215a0f8
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-08-21 13:41:45 -0300

    Fix views and controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    @posts = @user.posts
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 end
