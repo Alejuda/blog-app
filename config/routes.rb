43e3dcd5fc36490fc2689c0083c235bc6ef7cbea
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-08-24 12:32:21 -0300

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
-      resources :comments, only: [:new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:create, :new]
       resources :likes, only: [:create]
     end
   end
