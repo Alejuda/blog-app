b6da854cdaaf4debd9e91185a14ab4ed20abb0bf
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-08-28 15:53:59 -0300

    Add test for user index page

---
+++ spec/views/users_index_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'users/index', type: :system do
+  describe 'index page' do
+    let!(:user1) do
+      User.create(name: 'nacho', photo: 'https://th.bing.com/th/id/OIP.xlTxIX_i9UIgOJtZCd01jwHaHa?pid=ImgDet&rs=1', bio: 'teacher from argentina',
+                  posts_counter: 0)
+    end
+    let!(:user2) do
+      User.create(name: 'gabriel', photo: 'https://th.bing.com/th/id/OIP.xlTxIX_i9UIgOJtZCd01jwHaHa?pid=ImgDet&rs=1', bio: 'teacher from chile',
+                  posts_counter: 1)
+    end
+
+    before do
+      visit users_path
+    end
+
+    it 'displays the username of all users' do
+      expect(page).to have_selector('h2', text: 'nacho')
+      expect(page).to have_selector('h2', text: 'gabriel')
+    end
+
+    it 'displays the profile picture for each user' do
+      expect(page).to have_selector('img[alt="Profile photo"]', count: 2)
+    end
+
+    it 'displays the number of posts each user has written' do
+      expect(page).to have_selector('h4', text: 'Number of posts: 0')
+      expect(page).to have_selector('h4', text: 'Number of posts: 1')
+    end
+
+    it 'redirects to the user show page' do
+      visit users_path
+      click_link user1.name
+
+      expect(page).to have_current_path(user_path(user1))
+    end
+  end
+end
--- test/system/user_test.rb
+++ test/system/user_test.rb
@@ -1,9 +1,18 @@
 require 'application_system_test_case'
 
 class UsersTest < ApplicationSystemTestCase
-  test 'display users index' do
+  setup do
+    User.create(name: "nacho", photo: "https://th.bing.com/th/id/OIP.xlTxIX_i9UIgOJtZCd01jwHaHa?pid=ImgDet&rs=1",bio: "test bio", posts_counter: 0)
+    User.create(name: "gabriel", photo: "https://th.bing.com/th/id/OIP.xlTxIX_i9UIgOJtZCd01jwHaHa?pid=ImgDet&rs=1",bio: "test bio 2", posts_counter: 0)
+  end
+  test 'display all usernames' do
+    
     visit users_path()
-    assert_text 'name1'
-    assert_text 'name2'
+    assert_text 'nacho'
+    assert_text 'gabriel'
+
+  end
+  test 'display all user images' do
+    assert_selector('img', class: 'user-photo', count: 2)
   end
 end
