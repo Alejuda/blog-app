b79d154ce2b47fefdfea0cca1c44ece1a9624477
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-08-30 14:25:10 -0300

    Fix foreing key error when deleting post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes, foreign_key: :post_id, dependent: :destroy
-  has_many :comments, foreign_key: :post_id
+  has_many :comments, foreign_key: :post_id, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_user_posts_counter
-  before_destroy :delete_associated_likes
+  before_destroy :delete_associated_likes_comments
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
@@ -20,8 +20,9 @@ class Post < ApplicationRecord
     author.update(posts_counter: author.posts.count)
   end
 
-  def delete_associated_likes
+  def delete_associated_likes_comments
     self.likes.destroy_all
+    self.comments.destroy_all
     author.decrement!(:posts_counter)
   end
 
